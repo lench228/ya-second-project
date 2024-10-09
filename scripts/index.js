e912bde2a2a2d319444869d8040cf6f1f5d341e1
Lench <leonid.gneushev@gmail.com>
2024-10-09 11:25:34 +0500

    render init cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,29 @@
+import {initialCards} from './cards.js';
+
 // @todo: Темплейт карточки
-const cardTemplate = document.getElementById('card').content;
-const cardElement = cardTemplate.content.cloneNode(true);
+const cardTemplate = document.querySelector('#card-template');
+
 // @todo: DOM узлы
+const container = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
-function createCard(){
+function createCard(card) {
 
+    const cardElement = cardTemplate.content.cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    cardImage.src = card.link;
+    cardTitle.textContent = card.name;
+    return cardElement;
 }
+
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
 
+initialCards.forEach((card) => {
+    const newCard = createCard(card);
+    container.appendChild(newCard);
+});
 
