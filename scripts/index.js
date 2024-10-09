672102b5a53e28507bd410f5969debeb634f88d0
Lench <leonid.gneushev@gmail.com>
2024-10-09 11:02:56 +0500

    initial

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// @todo: Темплейт карточки
+const cardTemplate = document.getElementById('card').content;
+const cardElement = cardTemplate.content.cloneNode(true);
+// @todo: DOM узлы
+
+// @todo: Функция создания карточки
+function createCard(){
+
+}
+// @todo: Функция удаления карточки
+
+// @todo: Вывести карточки на страницу
+
+
